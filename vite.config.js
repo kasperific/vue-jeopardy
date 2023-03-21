eb22ad4fa0df19adb157b01439834eb9e9864e13
Sharon Kasper <sharon@sharon-kasper.com>
2023-03-21 16:03:27 -0700

    New files for upgrade

---
+++ vite.config.js
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite'
+import createVuePlugin from '@vitejs/plugin-vue'
+
+export default defineConfig({
+    resolve: {
+      alias: {
+        vue: '@vue/compat'
+      }
+    },
+    plugins: [
+      createVuePlugin({
+        template: {
+          compilerOptions: {
+            compatConfig: {
+              MODE: 2
+            }
+          }
+        }
+      })
+    ],
+    server: {
+      watch: {
+        usePolling: true,
+      }
+    }
+  })
+  
