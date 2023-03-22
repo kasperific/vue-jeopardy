2a92f16c1caa1150860d0695efb81aeff7d8a123
Sharon Kasper <sharon@sharon-kasper.com>
2023-03-22 00:39:09 -0700

    Added base to config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import createVuePlugin from '@vitejs/plugin-vue'
 
 export default defineConfig({
+    base: '/vue-jeopardy/',
     resolve: {
       alias: {
         vue: '@vue/compat'
